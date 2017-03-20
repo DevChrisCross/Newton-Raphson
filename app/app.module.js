1d1de8531f84ebc0a9797ab7b751f707472dc23d
DevChrisCross <Dev.ChrisMolina@gmail.com>
2017-03-20 11:06:43 +0800

    fully furnished UI and algorithm

--- app/app.module.js
+++ app/app.module.js
@@ -15,11 +15,12 @@
     .value('duScrollDuration', 1500)
     .value('duScrollOffset', 80)
     .value('duScrollBottomSpy', true)
-    .component("computationTable", {
-        templateUrl: "app/computation/computation.template.html",
-        controller: computationController,
-        controllerAs: "computeVm"
-    });
+    // .component("computationTable", {
+    //     templateUrl: "app/computation/computation.template.html",
+    //     controller: computationController,
+    //     controllerAs: "computeVm"
+    // })
+    .factory("NrmService", NrmService);
 
     configuration.$inject = ["$urlRouterProvider", "$stateProvider"];
     function configuration  ( $urlRouterProvider ,  $stateProvider ) {
@@ -44,15 +45,34 @@
 
     }
 
-    infoGraphicsController.$inject = [];
-    function infoGraphicsController() {
+    infoGraphicsController.$inject = ["$rootScope", "NrmService"];
+    function infoGraphicsController  ( $rootScope ,  NrmService ) {
         AOS.init();
         let self = this;
         self.setSection = setSection;
         self.isSection = isSection;
+        self.startApp = startApp;
+        self.displayEquation = displayEquation;
+        self.computeEquation = computeEquation;
 
         (function init() {
+            self.disconnected = typeof MathJax==='undefined';
+            self.start = false;  //default value false
             self.currentSection = 'home';
+            self.navMenu = [
+                {
+                    link: "home",
+                    name: "Home"
+                },
+                {
+                    link: "info",
+                    name: "Discover Us"
+                },
+                {
+                    link: "start",
+                    name: "Start Now"
+                }
+            ];
             self.members = [
                 {
                     photoRef: "paul",
@@ -90,6 +110,47 @@
                     name: "Molina, Christian Noel C."
                 },
             ];
+
+            self.initialPlot = {
+                title: "Function Plot",
+                width: 750,
+                height: 500,
+                grid: true,
+                target: '#initialPlot',
+                xAxis: {label: "X-Axis"},
+                yAxis: {label: "Y-Axis"},
+                tip: {
+                    xLine: true,    // dashed line parallel to y = 0
+                    yLine: true,    // dashed line parallel to x = 0
+                    renderer: function (x, y, index) {
+                        // the returning value will be shown in the tip
+                    }
+                }
+            };
+
+            self.computationPlot = {
+                title: "Newton-Raphson Plot",
+                width: 750,
+                height: 500,
+                grid: false,
+                target: '#finalPlot',
+                xAxis: {label: "X-Axis"},
+                yAxis: {label: "Y-Axis"},
+                tip: {
+                    xLine: true,    // dashed line parallel to y = 0
+                    yLine: true,    // dashed line parallel to x = 0
+                    renderer: function (x, y, index) {
+                        // the returning value will be shown in the tip
+                    }
+                },
+                data: [],
+                annotations: []
+            };
+
+            let a = functionPlot(self.initialPlot);
+            let b = functionPlot(self.computationPlot);
+            a.addLink(b);
+            b.addLink(a);
         })();
 
         function setSection(string) {
@@ -100,20 +161,72 @@
             console.log(self.currentSection === string);
             return self.currentSection === string;
         }
+
+        function startApp() {
+            self.start = true;
+            self.disconnected = typeof MathJax==='undefined';
+        }
+        
+        function displayEquation() {
+            MathJax.Hub.Queue(['Text', MathJax.Hub.getAllJax('equationTex')[0], math.parse("f(x)=" + self.equation).toTex({parenthesis: "keep"})]);
+            self.initialPlot.data = [{
+                sampler: "builtIn",
+                graphType: "polyline",
+                fn: self.equation,
+                derivative: {
+                    fn: math.derivative(self.equation, 'x').toString(),
+                    updateOnMouseMove: true
+                }
+            }];
+            console.log(self.initialPlot.data);
+            functionPlot(self.initialPlot);
+        }
+
+        function computeEquation(){
+            self.computationPlot.data = [];
+            self.computationPlot.annotations = [];
+            let response = NrmService.nrm(self.equation, self.initialX);
+            self.nrmData = response;
+
+            for(let i = 0; i < response.table.length; i++){
+                self.computationPlot.data.push({
+                    sampler: "builtIn",
+                    graphType: "polyline",
+                    fn: response.data.eFunction,
+                    derivative: {
+                        fn: response.data.eDerivative,
+                        x0: response.table[i].valueX
+                    }
+                });
+                self.computationPlot.annotations.push({
+                    x: response.table[i].valueX,
+                    // text: "x = " + response.table[i].valueX
+                });
+            }
+
+            self.computationPlot.data.push({
+                sampler: "builtIn",
+                graphType: "polyline",
+                fn: response.data.eFunction,
+                derivative: {
+                    fn: response.data.eDerivative,
+                    updateOnMouseMove: true
+                }
+            });
+
+            functionPlot(self.computationPlot);
+        }
     }
 
-    computationController.$inject = [];
-    function computationController() {
-        let self = this;
+    NrmService.$inject = [];
+    function NrmService() {
+        let self = {};
+
         self.radians = radians;
         self.roundOff = roundOff;
         self.nrm = nrm;
 
-        (function init() {
-            self.equationInput = "sin(x)+cos(x)";
-            self.initialGuess = "0.5";
-            self.iteration = "5";
-        })();
+        return self;
 
         function radians(degrees) {
             return (degrees * 180) / Math.PI;
@@ -123,67 +236,99 @@
             return Math.round(number*1000000)/1000000;
         }
 
-        function nrm() {
+        function nrm(equation, initialValue) {
             let parser = math.parser();
             let table = [];
-            let obj = {
-                initialOfX: self.initialGuess,
-                rad: rad,
-                func: func,
-                funcDeri: funcDeri,
-                deriOfX: deriOfX,
-                answer: answer
-            };
+            let approximation = 100;
+            let data = {};
+            data.eFunction = equation;
+            data.eDerivative = math.derivative(equation, 'x').toString();
 
-            let i = 0;
-            let approx = [];
+            let currentData = {};
+            currentData.valueX = initialValue;
 
-            // convert angle to radian
-            obj.rad = self.radians(obj.initialOfX);
+            for(let iteration = 0; approximation > .01; iteration++){
 
-            // f(x)
-            obj.func = self.equationInput.replace(/x/gi, obj.initialOfX);
-            obj.func = parser.eval(obj.func);
+                currentData.iteration = iteration;
+                currentData.functionX =  parser.eval(data.eFunction.replace(/x/gi, currentData.valueX)).toPrecision(5);
 
-            // Derivative of f(x)
-            obj.deriOfX = math.derivative(self.equationInput,'x');
+                currentData.derivativeX = parser.eval(data.eDerivative.replace(/x/gi, currentData.valueX)).toPrecision(5);
+                currentData.valueXPlusOne = self.roundOff((currentData.valueX - currentData.functionX / currentData.derivativeX));
 
-            // initializing value of x
-            obj.funcDeri = obj.deriOfX.toString().replace(/x/gi, obj.rad);
-            obj.funcDeri = parser.eval(obj.funcDeri);
+                table.push(currentData);
+                if(table[iteration-1] !== undefined){
+                    currentData.approxError = (Math.abs( (table[iteration].valueX - table[iteration-1].valueX) / table[iteration].valueX ) * 100).toFixed(2);
+                    approximation = currentData.approxError;
+                }else {
+                    currentData.approxError = "undefined";
+                }
 
-            // NRM
-            obj.answer = parser.eval(obj.initialOfX + '+' + obj.func + '/' + obj.funcDeri);
-            table[i] = table.push(obj);
-            i++;
-            let holder =  obj.func;
+                let newInitialX = currentData.valueXPlusOne;
+                currentData = {};
+                currentData.valueX = newInitialX;
+            }
 
-            do {
-                obj = {};
-                obj.initialOfX = holder;
-                // convert angle to radian
-                obj.rad = self.radians(obj.initialOfX);
+            return { data: data, table: table };
 
-                // f(x)
-                obj.func = self.equationInput.replace(/x/gi, obj.initialOfX);
-                obj.func = parser.eval(obj.func);
+            // let obj = {
+            //     initialOfX: self.initialGuess,
+            //     rad: rad,
+            //     func: func,
+            //     funcDeri: funcDeri,
+            //     deriOfX: deriOfX,
+            //     answer: answer
+            // };
 
-                // Derivative of f(x)
-                obj.deriOfX = math.derivative(self.equationInput,'x');
 
-                // initializing value of x
-                obj.funcDeri = deriOfX.toString().replace(/x/gi, obj.rad);
-                obj.funcDeri = parser.eval(obj.funcDeri);
-
-                // NRM
-                obj.answer = parser.eval(obj.initialOfX + '+' + obj.func + '/' + obj.funcDeri);
-
-                table[i] = table.push(obj);
-                obj.approx = (table[i]['initialOfX'] - table[i - 1]['initialOfX']) / table[i]['initialOfX'];
-
-                holder = func;
-                i++;
-            } while(approx<.01);
+            // let i = 0;
+            // let approx = [];
+            //
+            // // convert angle to radian
+            // obj.rad = self.radians(obj.initialOfX);
+            //
+            // // f(x)
+            // obj.func = self.equationInput.replace(/x/gi, obj.initialOfX);
+            // obj.func = parser.eval(obj.func);
+            //
+            // // Derivative of f(x)
+            // obj.deriOfX = math.derivative(self.equationInput,'x');
+            //
+            // // initializing value of x
+            // obj.funcDeri = obj.deriOfX.toString().replace(/x/gi, obj.rad);
+            // obj.funcDeri = parser.eval(obj.funcDeri);
+            //
+            // // NRM
+            // obj.answer = parser.eval(obj.initialOfX + '+' + obj.func + '/' + obj.funcDeri);
+            // table[i] = table.push(obj);
+            // i++;
+            // let holder =  obj.func;
+            //
+            // do {
+            //     obj = {};
+            //     obj.initialOfX = holder;
+            //     // convert angle to radian
+            //     obj.rad = self.radians(obj.initialOfX);
+            //
+            //     // f(x)
+            //     obj.func = self.equationInput.replace(/x/gi, obj.initialOfX);
+            //     obj.func = parser.eval(obj.func);
+            //
+            //     // Derivative of f(x)
+            //     obj.deriOfX = math.derivative(self.equationInput,'x');
+            //
+            //     // initializing value of x
+            //     obj.funcDeri = deriOfX.toString().replace(/x/gi, obj.rad);
+            //     obj.funcDeri = parser.eval(obj.funcDeri);
+            //
+            //     // NRM
+            //     obj.answer = parser.eval(obj.initialOfX + '+' + obj.func + '/' + obj.funcDeri);
+            //
+            //     table[i] = table.push(obj);
+            //     obj.approx = (table[i]['initialOfX'] - table[i - 1]['initialOfX']) / table[i]['initialOfX'];
+            //
+            //     holder = func;
+            //     i++;
+            // } while(approx<.01);
 
 
         }
